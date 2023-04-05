282dc993a79254c88a37778ec824d9d9ab56f7ac
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-04-05 02:41:27 +0200

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+$(function () {
+// SHOW PRELOADER ON LOAD 
+	$(window).load(function() {
+		$("#loader").fadeOut("slow");
+	})
+
+
+
+$("selector").click(function (e) { 
+	e.preventDefault();
+	
+});
+
+
+})
+
+
